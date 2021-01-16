07312ed8de27c7fe9e699d0bc2b5a9c712c03045
Aryan Shrivastava <aryanshrivastava.3@gmail.com>
2021-01-16 16:08:42 +0530

    Complete VC

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        playSound(for: sender.currentTitle!)
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { (timer) in
+            sender.alpha = 1
+            timer.invalidate()
+        }
+    }
+    
+    func playSound(for title: String){
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        do{
+            player = try AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }catch{
+            fatalError("Error in playing Sound: \(error.localizedDescription)")
+        }
+    }
 }
 
